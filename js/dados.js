01e404d7614b10bdfb6e094ce3d131e9c7630ce1
Jaderm1 <jadermaciel1@gmail.com>
2023-08-06 21:24:53 -0300

    criado a estrutura de login e base de dados

---
+++ js/dados.js
@@ -0,0 +1,12 @@
+
+// nome do arquivo - dados.js
+const dadosUsuario = `
+  [
+    {
+      "nome": "Admin",
+      "email": "teste@email.com",
+      "senha" : 1234
+    }
+  ]
+`
+alert('Dados carregados com sucesso')
